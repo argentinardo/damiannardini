a70f57531d98a81c2d8353cd078b4ed764f5bf0c
damian nardini <damiannardini@gmail.com>
2020-06-04 19:06:38 +0200

    add vue.config.js for load main.scss in the project

--- src/components/CardConsole.vue
+++ src/components/CardConsole.vue
@@ -1,13 +1,32 @@
 <template>
-    <div></div>
+  <div class="card">
+    <p class="card__paragraph">
+      {{ paragraph }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-    
-}
+  name: "card",
+  data: function() {
+    return {
+      paragraph:
+        "dsfasdf sdf df df adfdfsdfewrt sgf gfds gsdfg sdf gdfg fds gsfg lorem"
+    };
+  }
+};
 </script>
 
 <style lang="scss">
-
+.card {
+  background-color: $gray--darker;
+  width: 33%;
+  height: 30vh;
+  margin: auto;
+  border-radius: 3px;
+}
+.card__paragraph {
+  color: $gray--lighter;
+}
 </style>
