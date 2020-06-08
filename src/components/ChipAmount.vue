f3945f074b3f28877cfc957d1635183e626942d8
damian nardini <damiannardini@gmail.com>
2020-06-08 14:47:36 +0200

    svg path included in the model (dry)

--- src/components/ChipAmount.vue
+++ src/components/ChipAmount.vue
@@ -2,13 +2,13 @@
   <div class="chip__amount">
     <span class="chip__amount amount__number">{{ amount }}</span>
     <svg class="amount__chart amount__chart--fixed">
-      <path d=" M 14 26 A 12 12 90 0 1 14 2" />
+      <path :d="svgPath" />
     </svg>
     <svg
       class="amount__chart amount__chart--rotated"
       :style="'transform:rotate(' + percent + 'deg)'"
     >
-      <path d=" M 14 26 A 12 12 90 0 1 14 2" />
+      <path :d="svgPath" />
     </svg>
   </div>
 </template>
@@ -16,6 +16,11 @@
 export default {
   name: "ChipAmount",
   props: ["amount"],
+  data() {
+    return {
+      svgPath: " M 14 26 A 12 12 90 0 1 14 2"
+    };
+  },
   computed: {
     percent() {
       return 180 - this.amount * 3.6;
