6e30e5263dc8645f504f73bb2792a468dceec317
damian nardini <damiannardini@gmail.com>
2020-06-11 02:11:01 +0200

    commit WIP, I need to find a solution for paragraph in a card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,9 +3,9 @@
     <h1 class="card__title" v-if="title">
       <slot name="title"></slot>
     </h1>
-    <p v-if="paragraph" class="card__paragraph">
+    <span v-if="paragraph" class="card__paragraph">
       <slot name="paragraph"></slot>
-    </p>
+    </span>
     <slot v-if="chips" name="chips"></slot>
   </div>
 </template>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -2,7 +2,19 @@
   <div class="about-me">
     <card :paragraph="true">
       <template slot="paragraph">
-        Sobre mí
+        Nací en Argentina, actualmente resido en Barcelona. Trabajo en
+        tecnologías web desde hace más de 10 años actualmente como front-end /
+        UI. Esto me exigió una constante capacitación tanto a cargo de las
+        empresas donde trabajé como de forma personal. He incursionado en el
+        diseño gráfico y digital creando piezas de animación tanto para web, cd
+        interactivos y video. También cuento con formación en ilustración y
+        animación 3D y tradicional. Cada nuevo proyecto es un desafío al cual me
+        gusta abordar con mi experiencia e incursionando nuevas posibilidades.
+        Siempre estoy abierto a nuevos retos,<router-link
+          class="about-me__link"
+          to="/Contact"
+          >hablamos?</router-link
+        >
       </template>
     </card>
   </div>
@@ -18,3 +30,8 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+.about-me__link {
+  color: $third;
+}
+</style>
