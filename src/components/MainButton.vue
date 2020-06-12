46d142706820f01865f27b2892d60aea6d64b646
damian nardini <damiannardini@gmail.com>
2020-06-12 17:17:52 +0200

    main-button component added

---
+++ src/components/MainButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-button">
+    <a :href="link" class="main-button__link">{{ text }}</a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["text", "link"]
+};
+</script>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,17 +4,20 @@
       <p class="card__paragraph">
         Contacto
       </p>
+      <main-button text="Enviar" link="#"></main-button>
     </card>
   </div>
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import MainButton from "@/components/MainButton.vue";
 
 export default {
   name: "Contacto",
   components: {
-    Card
+    Card,
+    MainButton
   }
 };
 </script>
