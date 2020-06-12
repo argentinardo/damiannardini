0824d61da3724de53cb5e1546103faa24ad48ace
damian nardini <damiannardini@gmail.com>
2020-06-12 15:58:55 +0200

    added styling to tools used in about Web section

--- src/views/AboutWeb.vue
+++ src/views/AboutWeb.vue
@@ -8,28 +8,27 @@
         de la empresa que me contrataba.<br />
         Ya que mi aporte fue siempre una tarea especifica del proyecto, no
         cuento con un porfolio propio para mostrar mis trabajos.<br />
-        Es por eso que he decidido hacer esta web, para mostrar mi metodología
-        de trabajo puedes ver todo el código y el proceso en
+        Es por eso que he decidido hacer esta web desde el diseño hasta el
+        desarrollo, para mostrar mi metodología de trabajo puedes ver todo el
+        código y el proceso en
         <a
           class="card__link"
           href="https://github.com/argentinardo/damiannardini"
           target="_blank"
           >github</a
         >.<br />
-        Pretendí plasmar todo lo que aprendí estos años de trabajo más nuevos
-        conocimientos que fui adquiriendo en el proceso.<br />
+        Pretendí plasmar todo lo aprendido estos años de trabajo más nuevos
+        conocimientos que fui adquiriendo durante proceso.<br />
         Espero que te agrade.<br />
       </p>
-      <p class="card__paragraph">
-        <span class="card__emphasis"> Software utilizado:</span> adobe XD,
-        Visual Studio Code<br />
+      <p class="card__cluster">
+        <span class="card__emphasis"> Software utilizado:</span> Adobe XD, VS
+        Code<br />
         <span class="card__emphasis"> Arquitectura css:</span> Bemit (BEM +
         itcss)<br />
-        <span class="card__emphasis"> Desarrrollo Javascript:</span> Vue.js +
-        vanilla js, eslint y prettify<br />
+        <span class="card__emphasis"> Desarrollo:</span> Vue.js + vanilla js<br />
         <span class="card__emphasis"> Empaquetador:</span> Webpack<br />
-        <span class="card__emphasis"> Manejo de versiones:</span> Git +
-        github<br />
+        <span class="card__emphasis"> Versionado:</span> Git + github<br />
         <span class="card__emphasis"> Hosting:</span> Netifly<br />
         <!-- agregar algo sobre el diseño y ver si es posible compartir el archivo XD -->
       </p>
