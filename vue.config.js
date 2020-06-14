7b028dac9f4e9434366a07976e8d7b5cd45432e4
damian nardini <damiannardini@gmail.com>
2020-06-14 12:01:51 +0200

    added prerender config for form

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
 module.exports = {
   css: {
     loaderOptions: {
@@ -5,5 +7,21 @@ module.exports = {
         prependData: `@import "@/sass/main.scss";`
       }
     }
+  },
+  configureWebpack: () => {
+    if (process.env.NODE_ENV !== "production") return;
+    return {
+      plugins: [
+        new PrerenderSPAPlugin(
+          // Absolute path to compiled SPA
+          path.resolve(__dirname, "dist"),
+          // List of routes to prerender
+          ["/"],
+          {
+            // options
+          }
+        )
+      ]
+    };
   }
 };
