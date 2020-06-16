3e604c5090059150ba49dc1c47148d4fb048957c
damian nardini <damiannardini@gmail.com>
2020-06-16 10:58:15 +0200

    form submition redirection

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -5,7 +5,7 @@
     data-netlify="true"
     data-netlify-honeypot="bot-field"
     class="contact__form"
-    action=""
+    action="/thanks"
   >
     <input type="hidden" name="form-name" value="web_message" />
     <label class="contact__label--hidden hidden"
