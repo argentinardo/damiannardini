5787f6cb45492c4745a6027a6d48ad134de749e7
damian nardini <damiannardini@gmail.com>
2020-06-21 02:09:27 +0200

    Drawing styling and layout added

---
+++ src/components/Tag.vue
@@ -0,0 +1,18 @@
+<template>
+  <div @click="activate" class="tag">
+    <span class="tag__text">
+      {{ text }}
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  name: "Tag",
+  props: ["text", "size"],
+  methods: {
+    activate() {
+      this.$emit("changeState(text)");
+    }
+  }
+};
+</script>
--- src/views/Drawing.vue
+++ src/views/Drawing.vue
@@ -1,20 +1,72 @@
 <template>
   <div class="Drawing">
+    <card flex="row">
+      <tag
+        v-for="category in categories"
+        :key="category.index"
+        :text="category"
+        size="normal"
+        @activate="changeState()"
+      ></tag>
+    </card>
     <card>
-      <p class="card__paragraph">
-        Ilustraciones
-      </p>
+      <img
+        class="drawing__image"
+        v-for="image in images"
+        :key="image.title"
+        :alt="image.title"
+        :src="image.url()"
+      />
     </card>
   </div>
 </template>
 
 <script>
 import Card from "@/components/Card.vue";
+import Tag from "@/components/Tag.vue";
 
 export default {
   name: "Drawing",
   components: {
-    Card
+    Card,
+    Tag
+  },
+  data() {
+    return {
+      categories: [
+        "ver todas",
+        "historietas",
+        "digital",
+        "caricaturas",
+        "objetos"
+      ],
+      images: [
+        {
+          url() {
+            return require("@/assets/images/drawings/aladin.jpg");
+          },
+          tag: ["comic"],
+          title: "Deseos",
+          description: "Dibujo digital en tableta"
+        },
+        {
+          url() {
+            return require("@/assets/images/drawings/arcade.jpg");
+          },
+          tag: ["objetos"],
+          title: "Arcade",
+          description: "Dibujo sobre mueble arcade"
+        },
+        {
+          url() {
+            return require("@/assets/images/drawings/gandhi.png");
+          },
+          tag: ["caricatura"],
+          title: "Nuevos tiempos",
+          description: "Caricatura en lapiz"
+        }
+      ]
+    };
   }
 };
 </script>
